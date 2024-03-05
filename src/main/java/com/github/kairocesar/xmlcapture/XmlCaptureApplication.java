fcd84e92a01ce5c16949734e922c16bd6f200a1b
kairoCesar <kairo8121@gmail.com>
2024-03-04 22:28:50 -0300

    initial commit

---
+++ src/main/java/com/github/kairocesar/xmlcapture/XmlCaptureApplication.java
@@ -0,0 +1,13 @@
+package com.github.kairocesar.xmlcapture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlCaptureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlCaptureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kairocesar/xmlcapture/XmlCaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kairocesar.xmlcapture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlCaptureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
